cde2ebe486596e0d122e689e1ba21725e5c915b0
Dpaace <dipeshnepali767@gmail.com>
2024-06-05 17:57:50 +0545

    Updates in the Landing page

--- src/pages/Sections/Landing/Landing.jsx
+++ src/pages/Sections/Landing/Landing.jsx
@@ -1,9 +1,45 @@
 import './Landing.css'
+import Profile from '../../../assets/Profile.png';
 
 export const Landing = () => {
-    return(
-        <section id="landing" className=''>
-            <h1>Landing</h1>
+    return (
+        <section id="landing" className='bg-green-900 text-white h-lvh'>
+            <div className='relative max-w-screen-xl mx-auto pt-10 flex flex-row '>
+                <div className="absolute top-28 z-40">
+                    <h1 className="text-[156px] font-bold leading-[140px]">Dipesh<br />Nepali</h1>
+                    <br />
+                    <br />
+                    <br />
+                    <p className="text-xl font-semibold">
+                        SOFTWARE<br />
+                        <span className="relative">
+                            ENGINEER
+                            <span className="absolute left-0 top-6 w-8 h-1 bg-coral inline-block"></span>
+                        </span>
+                    </p>
+                </div>
+                <div className='relative bg-pink-200 h-[488px] w-[380px] mt-10 rounded-t-[200px] ml-[360px] z-30'>
+                    <img src={Profile} alt="Profile" className='h-full w-full object-cover' />
+                </div>
+                <div className='bg-red-500 w-2/5 items-end'>
+                    <p className="text-3xl font-semibold text-right">
+                        YEARS<br />
+                        EXPERIENCE
+                    </p>
+                </div>
+            </div>
+            <div className='bg-black h-56'>
+                <div className='max-w-screen-xl mx-auto grid'>
+                    Scroll Down
+                </div>
+            </div>
         </section>
+
+
+
+
+
     )
 }
+
+
