ae15439e7b1de2703effb19cfac7b463cda143d2
Dpaace <dipeshnepali767@gmail.com>
2024-06-10 17:38:38 +0545

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import { loadEnv } from 'dotenv'
-
-// const env = loadEnv();
-import { config } from 'dotenv';
-
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/dipeshnepali.github.io/"
 })
