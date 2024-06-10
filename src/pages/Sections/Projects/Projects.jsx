a0a0636c50d5a1e032d44a95d7276b6355831c31
Dpaace <dipeshnepali767@gmail.com>
2024-06-10 12:19:27 +0545

    Project Section Completed

--- src/pages/Sections/Projects/Projects.jsx
+++ src/pages/Sections/Projects/Projects.jsx
@@ -1,9 +1,16 @@
-import './Projects.css'
+import { ProjectCard } from "../../Components/Cards/ProjectCard"
 
 export const Projects = () => {
-    return(
-        <section id="projects">
-            <h1>Projects</h1>
+    return (
+        <section id="projects" className="bg-about text-black h-auto md:h-[700px]">
+            <div className="max-w-screen-xl mx-auto h-full px-4">
+                <h1 className='text-coral text-3xl md:text-5xl font-bold'>Projects</h1>
+                <h2 className='text-xl md:text-4xl mt-4'>My Projects showcases my expertise...</h2>
+                <div className="max-w-screen-xl bg-card h-[380px] md:h-[500px] rounded-3xl overflow-x-scroll whitespace-nowrap overflow-y-hidden">
+                    <ProjectCard />
+                </div>
+            </div>
+
         </section>
     )
 }
