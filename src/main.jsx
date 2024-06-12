a178e48f85699285ec0186c42659a4db6009e8c7
Dpaace <dipeshnepali767@gmail.com>
2024-06-12 12:24:03 +0545

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Dpaace.github.io">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
